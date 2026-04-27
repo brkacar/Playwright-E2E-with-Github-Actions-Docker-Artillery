8bb1561bf61d75b42c24c7c5128ba49921cdb764
Berk Acar <brkacaran@gmail.com>
2026-04-27 09:44:16 +0200

    Set Global Timeouts in playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -1,7 +1,12 @@
 const config = {
   timeout: 60000,
+  expect: {
+    timeout: 15000, // toBeVisible() etc. will wait up to 15s
+  },
   reporter: [['html', { outputFolder: 'playwright-report', open: 'never' }]],
   use: {
+    actionTimeout: 15000,
+    navigationTimeout: 30000,
     screenshot: 'only-on-failure',
     browserName: 'firefox',
     headless: true
